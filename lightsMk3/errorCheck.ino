059821ab6280593d44977325fba36e009eed12ac
TurnipAnnihilation <jamiealexanderspencer@gmail.com>
2016-03-14 20:57:20 +0000

    Added files via upload

---
+++ lightsMk3/errorCheck.ino
@@ -0,0 +1,28 @@
+int checkPin(int pinBuffer, int lnNum){
+  Serial.begin(9600); //open serial port
+  int pinDiff;
+
+  pinDiff = pinUpperLed - pinLowerLed;
+  pinBuffer--;
+  
+  if(pinBuffer > pinDiff){
+    Serial.print("Buffer Overflow: ");
+    Serial.println(lnNum);
+    return 1;
+  }
+
+  if(pinBuffer < pinDiff){
+    Serial.print("Buffer Underflow: ");
+    Serial.println(lnNum);
+    return 1;
+  }
+
+  if(pinUpperLed == (pinLowerLed + pinBuffer)){
+    Serial.print("Check Success: ");
+    Serial.println(lnNum);
+    return 0;
+  }
+}
+
+
+
---
+++ lightsMk3/lightsMk3.ino
@@ -0,0 +1,52 @@
+//#include <Arduino.h>
+int led[12]; //size of array = LEDNUM * 3 
+int errorStatus = 0;
+const int pinLowerLed = 30, pinUpperLed = 41, pinBuffer = 12;
+int i;
+
+void setup(){
+  Serial.begin(9600);
+  for(i = 0; i < pinBuffer; i++){
+    led[i] = i + pinLowerLed;
+    pinMode(led[i], OUTPUT);
+    delay(1);
+  }
+  //checkPin[pinLowerLed, pinUpperLed, led[pinBuffer], lineNumber]
+  errorStatus = checkPin(led[pinBuffer], 11);
+}
+
+void loop() {
+  if (errorStatus > 0){
+    programFail();
+    //Serial.println(" err ");
+  }  
+}
+
+void gameStart(){
+  for(i = 0; i <= 12; i++){
+    digitalWrite(led[i],HIGH);
+    delay(5000);
+  }
+  for(i = 12; i >= 0; i--){
+    digitalWrite(led[i],LOW);
+    delay(5000);
+  }
+}
+
+void programFail(){
+  int ledIndex;
+  int ledSel;
+  int k;
+  
+  ledIndex = (pinBuffer / 3) - 1;
+  for(i = 0; i <= ledIndex; i++){
+    ledSel = (i * 3) + 2;
+    digitalWrite(led[ledSel], HIGH);
+  }
+  delay(2000);
+  for(i = 0; i <= ledIndex; i++){
+    ledSel = (i * 3) + 2;
+    digitalWrite(led[ledSel], LOW);
+  }
+  delay(2000);
+}
